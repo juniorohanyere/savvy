af03ceba3564257ee1352c750b97af4e52f946ec
Twin J <junohanyere@gmail.com>
2023-04-14 19:39:46 +0100

    Modified, and low battery. See ya tomorrow. Even this rain

--- dht/dht_summary.c
+++ dht/dht_summary.c
@@ -1,3 +1,5 @@
+static int score = get_score();
+
 /**
  * get_percentage - function to calculate the percentage from score
  *
@@ -6,8 +8,15 @@
 
 float get_percentage(int num)
 {
-	int score = get_score();
 	float percentage = ((float) score / num) * 100;
 
 	return (percentage);
+}
+
+char *get_summary(void)
+{
+	float percentage = get_percentage();
+
+	if (percentage < 40)
+		char *fail[];
 }
