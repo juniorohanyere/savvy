cdb8adc50ead08c5b49907ccf2122193c880b6c8
Twin J <junohanyere@gmail.com>
2023-04-14 16:08:59 +0100

    new

---
+++ dht/dht_date.c
@@ -0,0 +1,52 @@
+#include <time.h>
+
+/**
+ * day_of_month - function to get current day of month
+ *
+ * Description: day of week is formatted in 'Www'
+ *
+ * Return: return the current day of the week
+*/
+
+int day_of_month(void)
+{
+	time_t t = time(NULL);
+	struct tm date_time = *localtime(&t);
+	int _day_of_month = date_time.tm_mday;
+
+	return (_day_of_month);
+}
+
+/**
+ * month - function to get the current month of the year
+ *
+ * Description: month is formatted in 'Mmm'
+ *
+ * Return: return the current month of the year
+*/
+
+int month(void)
+{
+	time_t t = time(NULL);
+	struct tm date_time = *localtime(&t);
+	int _month = date_time.tm_mon + 1;
+
+	return (_month);
+}
+
+/**
+ * year - function to get the current year
+ *
+ * Description: year is in four digits, formatted in 'YYYY'
+ *
+ * Return: return the current year
+*/
+
+int year(void)
+{
+	time_t t = time(NULL);
+	struct tm date_time = *localtime(&t);
+	int _year = date_time.tm_year + 1900;
+
+	return (_year);
+}
---
+++ dht/dht_time.h
@@ -0,0 +1,8 @@
+#ifndef DHT_TIME_H
+#define DHT_TIME_H
+
+int seconds(void);
+int minute(void);
+int hour(void);
+
+#endif	/* DHT_TIME_H */
