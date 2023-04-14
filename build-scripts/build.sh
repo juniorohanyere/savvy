77316838f8b081fcd5223136d6646be6221755f0
Twin J <junohanyere@gmail.com>
2023-04-14 16:32:08 +0100

    Changed dht_lib.c to dht_questions.c

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # script to compile and link the projects source files
-gcc ../dht/dht.c ../lib/dht_lib.c ../questions/dht_questions_c.c ../answers/dht_answers_c.c -o ../dht/dht.out
+gcc ../dht/dht.c ../dht/dht_questions.c ../questions/dht_questions_c.c ../answers/dht_answers_c.c -o ../dht/dht.out
