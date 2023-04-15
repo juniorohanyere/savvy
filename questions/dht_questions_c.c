6a67f8475022faa6b592cf9b5ada45ef9523eabb
Twin J <junohanyere@gmail.com>
2023-04-15 11:13:24 +0100

    Added new function, 'get_length

--- questions/dht_questions_c.c
+++ questions/dht_questions_c.c
@@ -112,3 +112,20 @@ const char *question(int num)
 {
 	return (questions[num]);
 }
+
+/**
+ * get_length - function to get the total length
+ *		of the questions array
+ *
+ * Description: should be used in place of the manual
+ *		assignment of range value in the
+ *		show_questions function in the
+ *		do_hard_things/dht/dht_questions.c file
+ *
+ * Return: return the length of the questions array
+*/
+
+int get_length(void)
+{
+	return (strlen(questions) - 1);	/* this doesn't seem right */
+}
