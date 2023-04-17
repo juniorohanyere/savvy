c5340441054b34a9153ac5c693322859d3b6f007
Twin J <junohanyere@gmail.com>
2023-04-17 11:55:00 +0100

    Created macro variables for colors

--- savvy/options/arg_2.c
+++ savvy/options/arg_2.c
@@ -17,22 +17,27 @@ void arg_2(char str[], char *str1[])
 {
 	if (strcmp(str1[1], "-h") == 0 || strcmp(str1[1], "--help") == 0)
 	{
-		/* display a list of useable command options */
+		/* display a list of usable command options */
 	}
 	else if (strcmp(str1[1], "-v") == 0 || strcmp(str1[1], "--version") == 0)
-		printf("savvy \033[0;32m%s\033[0m created by Twin J\n\n", str);
+	{
+		printf("\n");
+		printf("savvy " GREEN "%s " DEFAULT "created by " BOLD_BLUE "Twin J" DEFAULT "\n\n", str);
+	}
 	else if (strcmp(str1[1], "-S") == 0 || strcmp(str1[1], "--summary") == 0)
 		get_summary(get_percentage());
 	else
 	{
-		printf("Unknown option: \033[0;31m%s\033[0m\n", str1[1]);
+		printf("Unknown option: " RED "%s" DEFAULT, str1[1]);
+		printf("\n");
+
 		help_message();
 	}
 }
 
 void help_message(void)
 {
-	printf("Please use the \033[0;33m-h\033[0m ");
-	printf("or \033[0;33m--help\033[0m option ");
+	printf("Please use the " YELLOW "-h " DEFAULT);
+	printf("or " YELLOW "--help " DEFAULT "option ");
 	printf("to view a list of available commands and their use.\n\n");
 }
--- savvy/options/arg_2.h
+++ savvy/options/arg_2.h
@@ -1,6 +1,11 @@
 #ifndef ARG_2_H
 
 #define ARG_2_H
+#define BOLD_BLUE "\e[1;34m"
+#define DEFAULT "\e[0m"
+#define GREEN "\e[0;32m"
+#define RED "\e[0;31m"
+#define YELLOW "\e[0;33m"
 
 void arg_2(char str[], char *str1[]);
 void help_message(void);
