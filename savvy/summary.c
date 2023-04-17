4a7772558cfae7e33057e008d588cf52aeb7905d
Twin J <junohanyere@gmail.com>
2023-04-17 01:30:29 +0100

    Completed the get_summary function

--- savvy/summary.c
+++ savvy/summary.c
@@ -3,6 +3,8 @@ static int score = get_score();
 /**
  * get_percentage - function to calculate the percentage from score
  *
+ * @num: the score gotten from quiz taken
+ *
  * Return: return the percentage of the score
 */
 
@@ -13,10 +15,22 @@ float get_percentage(int num)
 	return (percentage);
 }
 
-char *get_summary(void)
-{
-	float percentage = get_percentage();
+/**
+ * get_summary - function to create a summary
+ *		 of the quiz the user has taken
+ *
+ * @num: fleat variable, the percentage gotten from score
+ *
+ * Return: return nothing
+*/
 
-	if (percentage <= 40)
-		char *fail[];
+void get_summary(float num)
+{
+
+	if (num <= 39)
+		printf("\033[0;31m%.f\033[0m", num);
+	else if (num >= 40 && num <= 69)
+		printf("\033[0;32m%.f\033[0m", num);
+	else
+		printf("\033[0;33m%.f\033[0m", num);
 }
---
+++ savvy/summary.h
@@ -0,0 +1,8 @@
+#ifndef SUMMARY_H
+
+#define SUMMARY_H
+
+float get_percentage(int num);
+void get_summary(float num);
+
+#endif	/* SUMMARY_H */
