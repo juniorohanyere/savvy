3667779d2e3f5e195e51f63211b933473df5dd61
Twin J <junohanyere@gmail.com>
2023-04-17 05:08:39 +0100

    Added new files to be compiled

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -3,4 +3,4 @@
 # script to compile and link the projects source files
 # generate an output file named savvy.out
 
-gcc ../savvy/savvy.c ../savvy/show_quiz.c ../quiz/c/quiz.c ../quiz_ref/c/ref.c -o ../savvy/savvy.out
+gcc ../savvy/savvy.c ../savvy/show_quiz.c ../quiz/c/quiz.c ../quiz_ref/c/ref.c ../savvy/options/arg_1.c ../savvy/options/arg_2.c ../savvy/summary.c -o ../savvy/savvy.out
