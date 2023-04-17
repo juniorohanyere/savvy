7ddddfd1aa180830fcf48d5da19b48598018fb85
Twin J <junohanyere@gmail.com>
2023-04-17 03:33:38 +0100

    Modified

--- savvy/summary.c
+++ savvy/summary.c
@@ -1,16 +1,21 @@
-static int score = get_score();
+#include "show_quiz.c"
 
 /**
  * get_percentage - function to calculate the percentage from score
  *
- * @num: the score gotten from quiz taken
- *
  * Return: return the percentage of the score
 */
 
-float get_percentage(int num)
+float get_percentage(void)
 {
-	float percentage = ((float) score / num) * 100;
+	int num_questions, score, *p;
+	float percentage;
+
+	p = get_num_score();
+	num_questions = p[0]
+	score = p[1];
+
+	percentage = ((float) score / num_questions) * 100;
 
 	return (percentage);
 }
@@ -19,7 +24,9 @@ float get_percentage(int num)
  * get_summary - function to create a summary
  *		 of the quiz the user has taken
  *
- * @num: fleat variable, the percentage gotten from score
+ * @num: float variable, the percentage gotten from score
+ *
+ * Description: (see get_percentage function from this file)
  *
  * Return: return nothing
 */
@@ -30,7 +37,26 @@ void get_summary(float num)
 	if (num <= 39)
 		printf("\033[0;31m%.f\033[0m", num);
 	else if (num >= 40 && num <= 69)
-		printf("\033[0;32m%.f\033[0m", num);
-	else
 		printf("\033[0;33m%.f\033[0m", num);
+	else
+		printf("\033[0;32m%.f\033[0m", num);
+}
+
+/**
+ * summary - function to print the result of the quiz
+ *	     as soon as the quiz has been taken
+ *
+ * Return: return nothing
+*/
+
+void summary(void)
+{
+	float percentage = get_percentage();
+
+	if (percentage <= 39)
+		printf("\033[0;31m%.f\033[0m", percentage);
+	else if (percentage >= 40 && <= 69)
+		printf("\033[0;33m%.f\033[0m", percentage);
+	else
+		printf("\033[0;32m%.f\033[0m", percentage);
 }
--- savvy/summary.h
+++ savvy/summary.h
@@ -2,7 +2,8 @@
 
 #define SUMMARY_H
 
-float get_percentage(int num);
+float get_percentage(void);
 void get_summary(float num);
+void summary(void);
 
 #endif	/* SUMMARY_H */
