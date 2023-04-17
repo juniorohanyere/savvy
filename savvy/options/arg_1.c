0ebf0c913c3707f36f2bcfc3391bfb56049c56a3
Twin J <junohanyere@gmail.com>
2023-04-17 03:57:13 +0100

    Renamed from home.*

---
+++ savvy/options/arg_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "arg_2.h"
+
+/**
+ * arg_1 - function to print a short description
+ *	  of the savvy program to the standard output
+ *
+ * Description: (see command function in ../savvy.c file)
+ *
+ * Return: return nothing
+*/
+
+void arg_1(void)
+{
+	printf("Welcome to savvy\n");
+	printf("Savvy is a command line program.\n");
+	printf("Aimed at increasing the tendency");
+	printf("At which programmers, expecially newbies learn");
+
+	help();
+}
---
+++ savvy/options/arg_1.h
@@ -0,0 +1,7 @@
+#ifndef HOME_H
+
+#define HOME_H
+
+void arg_1(void);
+
+#endif	/* HOME */
