51cdc5d03b895973964726bac055989599af9154
Twin J <junohanyere@gmail.com>
2023-04-17 03:44:50 +0100

    Modified

--- savvy/options/home.c
+++ savvy/options/home.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "help.h"
+
 /**
  * home - function to print a short description
  *	  of the savvy program to the standard output
@@ -16,7 +18,5 @@ void home(void)
 	printf("Aimed at increasing the tendency");
 	printf("At which programmers, expecially newbies learn");
 
-	printf("Please use the \033[0;32m'-h'\032[0m ");
-	printf("or \033[0;33m'--help'\033[0m option ");
-	printf("to view a list of available commands and their use.");
+	help_message();
 }
