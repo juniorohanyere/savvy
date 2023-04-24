2bf81d15877cbe511d5e29d3acd7f1a1b2f0b0fa
Twin J <junohanyere@gmail.com>
2023-04-24 19:36:00 +0100

    Deleted the buildscript directory, not needed since there's already a makefile

--- build-scripts/README.md
+++
@@ -1,1 +0,0 @@
-This directory contains all the scripts necessary to build or compile the savvy program
--- build-scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# script to compile and link the projects source files
-# generate an output file named savvy.out
-
-gcc ../savvy/savvy.c ../savvy/show_quiz.c ../quiz/c/quiz.c \
-../quiz_ref/c/ref.c ../savvy/options/arg_1.c ../savvy/options/arg_2.c \
-../savvy/summary.c -o ../savvy/savvy.out
