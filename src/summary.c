2e37568b95b40fa1bdad068c802ff1b345771fd2
Twin J <junohanyere@gmail.com>
2023-05-03 07:14:58 +0100

    modified

--- src/summary.c
+++ src/summary.c
@@ -8,6 +8,23 @@
 #include "colors.h"
 
 /**
+ * get_num_questions - function to get the number of questions
+ *		       entered by the user for a particular quiz
+ *
+ * Return: return the number of questions entered
+*/
+
+int get_num_questions(void)
+{
+	int num_questions;
+	int *p;
+
+	p = get_num_score();
+	num_questions = p[0];
+	return (num_questions);
+}
+
+/**
  * summary - function to print the result of the quiz
  *	     as soon as the quiz has been taken
  *
@@ -18,23 +35,27 @@ void summary(void)
 {
 	int sec = seconds(), min = minute(), hr = hour();
 	int day_of_mon = day_of_month(), mon = month(), yr = year();
-	int zero = is_zero(); /* score = get_score(); */
-	int positive = is_positive(), negative = is_negative();
-	float perentage = get_percentage();
+	int num_questions = get_num_questions(), score = get_score();
+	int positive = is_positive(), negative = is_negative(), zero = is_zero();
+	float percentage = get_percentage();
+	char *flag;
 
-	if (perentage <= 39)
-		printf(RED "%.f%%" DEFAULT "\n", perentage);
-	else if (perentage >= 40 && perentage <= 69)
-		printf(YELLOW "%.f%%" DEFAULT "\n", perentage);
+	if (percentage <= 39)
+		flag = RED;
+	else if (percentage >= 40 && percentage <= 69)
+		flag = YELLOW;
 	else
-		printf(GREEN "%.f%%" DEFAULT "\n", perentage);
+		flag = GREEN;
+
 	printf(GREEN);
 	printf("\n"
 		"|--------------------------------------------------------|\n"
 		"| |--------<SUMMARY>--------<%02d-%02d-%04d %02d:%02d:%02d>--------|\n"
 		"|--------------------------------------------------------|\n"
-		"| "
-		GREEN "+" DEFAULT "%-4d" RED "-" DEFAULT "%-4d" YELLOW "~" DEFAULT "%-4d ",
-		day_of_mon, mon, yr, hr, min, sec, positive, negative, zero);
+		"| " GREEN "~%-4d+" DEFAULT "%-4d" RED "-" DEFAULT
+		"%-4d" YELLOW ":" DEFAULT "%-4d " GREEN "=" DEFAULT
+		"%d\t\t\t%s%.2f%%" GREEN "\t |" DEFAULT "\n",
+		day_of_mon, mon, yr, hr, min, sec,
+		num_questions, positive, negative, zero, score, flag, percentage);
 	printf("\n");
 }
--- src/summary.h
+++ src/summary.h
@@ -2,6 +2,7 @@
 
 #define SUMMARY_H
 
+int get_num_questions(void);
 void summary(void);
 
 #endif	/* SUMMARY_H */
