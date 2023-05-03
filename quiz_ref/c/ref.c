ddc65288d880153d4853e39b2f204f26dce969aa
Twin J <junohanyere@gmail.com>
2023-05-03 13:27:13 +0100

    modified

--- quiz_ref/c/ref.c
+++ quiz_ref/c/ref.c
@@ -18,24 +18,25 @@
 
 const char *answers(int num, char str[])
 {
-	if ((num == 0 || num == 1 || num ==  3)
-	&& strcmp(str, "a") == 0)
+	if ((num == 0 || num == 1 || num ==  3 || num == 11 || num == 14)
+		&& strcmp(str, "a") == 0)
 		return (PASS);
-	else if ((num == 2 || num == 4 || num == 9)
-	&& strcmp(str, "b") == 0)
+	else if ((num == 2 || num == 4 || num == 9 || num == 12)
+		&& strcmp(str, "b") == 0)
 		return (PASS);
-	else if ((num == 5 || num == 6 || num == 7 || num == 8 || num == 10)
-	&& strcmp(str, "c") == 0)
+	else if ((num == 5 || num == 6 || num == 7 || num == 8 || num == 10 ||
+		num == 13)
+		&& strcmp(str, "c") == 0)
 		return (PASS);
 	else if (strcmp(str, "d") == 0)
-		if (num == 0 || num == 1 || num == 3)
+		if (num == 0 || num == 1 || num == 3 || num == 11)
 			return (LUCK_A);
-		else if (num == 2 || num == 4)
+		else if (num == 2 || num == 4 || num == 9 || num == 12)
 			return (LUCK_B);
 		else
 			return (LUCK_C);
 	else
-		if (num == 0 || num == 1 || num == 3)
+		if (num == 0 || num == 1 || num == 3 || num == 14)
 			return (FAIL_A);
 		else if (num == 2 || num == 4 || num == 9)
 			return (FAIL_B);
