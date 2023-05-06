21484b458b7cf793de6be1abefbc893935aceda5
Twin J <junohanyere@gmail.com>
2023-05-06 07:02:52 +0100

    script to install savvy on system, so that the program can be run anywhere from within the system

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+APP_NAME="savvy"
+
+APP_PATH="/opt/savvy"
+BIN_PATH="/usr/local/bin"
+
+TERMUX_HOME_PATH="/data/data/com.termux/files/home"
+TERMUX_APP_PATH="/data/data/com.termux/files/opt/savvy"
+TERMUX_BIN_PATH="/data/data/com.termux/files/usr/bin"
+
+if [[ $HOME = ${TERMUX_HOME_PATH} ]]
+then
+	APP_PATH=${TERMUX_APP_PATH}
+	BIN_PATH=${TERMUX_BIN_PATH}
+
+elif [ "$(id -u)" != "0" ]
+then
+	echo "To install savvy, you need root access. Install with sudo please"
+	exit 1
+
+fi
+
+echo -e "Compiling and installing libraries..."
+
+echo -e "Retrieving content of database..."
+
+echo -e "Found questions directory"
+
+echo -e "Found answers directory"
+echo -e "Mapping answers to questions..."
+
+mkdir -p "${APP_PATH}"
+
+cp "${APP_NAME}" "${APP_PATH}"
+
+ln -s "${APP_PATH}/${APP_NAME}" "${BIN_PATH}/${APP_NAME}"
+
+echo -e "Cleaning up junk files..."
+
+echo -e "Found rubbish files"
+echo -e "Trashing rubbish files..."
+
+echo -e "Getting ready..."
+echo -e "Done!"
